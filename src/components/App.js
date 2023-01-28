5a1f1b5ceb9ed9d4ec57417001d59b0597f11dd8
ilona <gogiashvili.ilona@gmail.com>
2023-01-28 00:34:36 -0500

    commit tree 9eba6db3864ac2a1702190bb08e590ddf3922cf8

--- src/components/App.js
+++ src/components/App.js
@@ -1,15 +1,52 @@
+
 import React, { useState } from "react";
 import AdminNavBar from "./AdminNavBar";
 import QuestionForm from "./QuestionForm";
 import QuestionList from "./QuestionList";
 
-function App() {
+function App({ onDeleteQuestion, question }) {
   const [page, setPage] = useState("List");
+  // const [questions, setQuestions] = useState([])
+  // console.log(questions)
+
+  // useEffect(() => {
+  //   fetch("http://localhost:4000/questions")
+  //     .then((resp) => resp.json())
+  //     .then((questions) => setQuestions(questions))
+  // }, []);
+
+
+  //   fetch("http://localhost:4000/questions", {
+  //     method: "POST",
+  //     headers: {
+  //       "Content-Type": "application/json",
+  //     },
+  //     body: JSON.stringify(itemData),
+  //   })
+  //     .then((r) => r.json())
+  //     .then((newItem) => onAddItem(newItem));
+  // }
+
+  // function handleDelete() {
+  //   fetch(`http://localhost:4000/questions/${question.id}`, {
+  //     method: "DELETE",
+  //   })
+  //     .then((r) => r.json())
+  //     .then(() => onDeleteQuestion(question));
+  // }
+  // function handleDeleteQuestion(deletedQuestion) {
+  //   const questionToDelete = questions.map((question) => question.id !== deletedQuestion.id)
+  //   setQuestions(updatedQuestion)
+
+
+  // }
+
 
   return (
     <main>
       <AdminNavBar onChangePage={setPage} />
       {page === "Form" ? <QuestionForm /> : <QuestionList />}
+      {/* <button className="delete" onClick={handleDelete}>Delete </button> */}
     </main>
   );
 }
